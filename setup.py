f0778962e3217a436f4a3ac056745b1070ce352b
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-10-12 19:47:51 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='askfm.py',
+      version='0.2',
+      description='Ask.fm crawler',
+      author='utgwkk',
+      author_email='utagawa.kiki@gmail.com',
+      url='https://github.com/utgwkk/askfm.py',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['requests', 'beautifulsoup4'],
+      license='MIT'
+      )
