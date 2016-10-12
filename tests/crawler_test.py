857c8e2ff0f71f3e2fce9e86d2c77010f2071c05
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-10-12 19:40:30 +0900

    Add tests

---
+++ tests/crawler_test.py
@@ -0,0 +1,57 @@
+import unittest
+import time
+import sys
+sys.path.append('.')
+import askfm
+
+class CrawlerTest(unittest.TestCase):
+    def setUp(self):
+        self.crawler = askfm.Crawler('ezoeryou')
+
+    def tearDown(self):
+        time.sleep(1)
+
+    def test_generate_url(self):
+        url = self.crawler.generate_url()
+        self.assertEqual(url, 'http://ask.fm/ezoeryou/answers/more?page=0')
+
+    def test_generate_url_with_page(self):
+        url = self.crawler.generate_url(5)
+        self.assertEqual(url, 'http://ask.fm/ezoeryou/answers/more?page=5')
+
+    def test_crawl_without_page(self):
+        datas = self.crawler.crawl()
+        self.assertIsInstance(datas, askfm.Answers)
+        for pair in datas:
+            self.assertIsInstance(pair, askfm.Pair)
+            self.assertIsInstance(pair.question, str)
+            self.assertIsInstance(pair.answer, str)
+            break
+
+    def test_crawl_with_page(self):
+        datas = self.crawler.crawl(1)
+        self.assertIsInstance(datas, askfm.Answers)
+        for pair in datas:
+            self.assertIsInstance(pair, askfm.Pair)
+            self.assertIsInstance(pair.question, str)
+            self.assertIsInstance(pair.answer, str)
+            break
+
+    def test_crawl_with_large_page_returns_empty_answers(self):
+        datas = self.crawler.crawl(100000000000000)
+        self.assertIsInstance(datas, askfm.Answers)
+        self.assertEqual(len(datas), 0)
+
+    def test_pager(self):
+        pager = self.crawler.pager()
+        datas = next(pager)
+        self.assertIsInstance(datas, askfm.Answers)
+        datas = next(pager)
+        self.assertIsInstance(datas, askfm.Answers)
+
+    def test_pager_stops_when_reached_max_page(self):
+        datas = self.crawler.pager(100000000000000)
+        self.assertRaises(StopIteration, next, datas)
+
+if __name__ == '__main__':
+    unittest.main()
