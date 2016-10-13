96d55aaef85a1e347be259b2cf3a4082d730ac48
theeluwin <theeluwin@gmail.com>
2016-10-14 00:37:15 +0900

    specifying html parser

--- askfm/parser.py
+++ askfm/parser.py
@@ -4,7 +4,7 @@ from .pair import Pair
 
 def parse(answers):
     '''parse given HTML from crawler.'''
-    soup = BeautifulSoup(answers)
+    soup = BeautifulSoup(answers, 'html.parser')
     retval = []
     for div in soup.select('.streamItem-answer'):
         question = div.select('.streamItemContent-question')[0].h2.string
