94f25ffad7ee2b3971bd23281ce4e749b78aa129
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-10-25 13:25:54 +0900

    Fix pair test

--- tests/pair_test.py
+++ tests/pair_test.py
@@ -6,10 +6,10 @@ from askfm import Pair
 
 class PairTest(unittest.TestCase):
     def setUp(self):
-        self.pair = Pair(u'ほほほほほ', u'質問ではない。')
+        self.pair = Pair(1, u'ほほほほほ', u'質問ではない。')
 
     def test_dict_pair(self):
-        self.assertDictEqual(dict(self.pair), {'question': u'ほほほほほ', 'answer': u'質問ではない。'})
+        self.assertDictEqual(dict(self.pair), {'id': 1, 'question': u'ほほほほほ', 'answer': u'質問ではない。'})
 
 if __name__ == '__main__':
     unittest.main()
