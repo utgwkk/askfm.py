507bf4d1f9f675e408cd850cc7c38be3d41edf75
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-10 02:02:35 +0900

    Add classifiers

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(name='askfm.py',
       url='https://github.com/utgwkk/askfm.py',
       packages=find_packages(exclude=['tests']),
       install_requires=['requests', 'beautifulsoup4'],
-      license='MIT'
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
       )
