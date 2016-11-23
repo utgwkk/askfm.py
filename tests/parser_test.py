599d4d3d17a1f7f100824d1fbb2686ad5e92e131
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-23 15:19:20 +0900

    Fix expected data

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -9,10 +9,10 @@ class ParserTest(unittest.TestCase):
     def test_parse_question(self):
         html = requests.get('http://ask.fm/ezoeryou/answers/more?page=2').text
         result = askfm.parser.parse(html)
-        self.assertEqual(len(result), 1)
+        self.assertEqual(len(result), 25)
         self.assertEqual(result[0].id, 135859194711)
         self.assertEqual(result[0].question, u'おいC++エヴァンゲリオン、C++ではなくSwiftを選ぶGoogleを許していいのか？')
-        self.assertEqual(result[0].answer, u'Swiftは\n罠がありそうで怖い。')
+        self.assertEqual(result[0].answer, u'Swiftは罠がありそうで怖い。')
 
 if __name__ == '__main__':
     unittest.main()
