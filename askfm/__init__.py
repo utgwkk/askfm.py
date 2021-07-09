64448e812f6960b871abd11838d671439a587bc3
utgwkk <utagawakiki@gmail.com>
2021-07-09 23:48:13 +0900

    print deprecation warning

--- askfm/__init__.py
+++ askfm/__init__.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.warn("askfm.py is no longer maintained, please consider using alternative library.", DeprecationWarning)
+
 from .crawler import Crawler
 from .answers import Answers
 from .pair import Pair
